ca4c117f3d6030448e961feb3eddf638fdeff968
Zainab Lakho <130156475+ZLakho@users.noreply.github.com>
2024-06-22 01:10:04 +0500

    Add files via upload

---
+++ third_class.ts
@@ -0,0 +1,70 @@
+// --------------- third class-----------
+// ________UNARY OPERATORS________
+// index++ = pre-increment
+// ++indexx = post-increment
+
+
+// let index : number = 10;
+// ++index
+// ++index
+// ++index
+// ++index
+// console.log(index)
+
+//    ------- another way
+
+// let number = 10;
+// ++number
+// ++number
+// ++number
+// console.log(number)
+
+
+// let index: number = 10;
+// ++index;
+// --index;
+// ++index;
+// --index;
+// --index;
+// --index;
+// console.log(index)
+
+
+// ----------QUIZ--------------
+// let index = 10;
+// let result = ++index + ++index + 7 + index - 5;
+// console.log(index); // 12
+// console.log(result); // 37
+
+// /______________SLIDE 128 HW ___________-
+// let weight_in_kg = 70;
+// let height_in_m = 1.75;
+// let BMI_cal = weight_in_kg/(height_in_m*height_in_m);
+// console.log(BMI_cal);
+
+
+// __________QUIZ_________
+// let a : number = 5;
+// let b : number = 2;
+// let c:number;
+// //   6  +  6  +  1  + 1  +  7 + 0 + 1 = 23 
+// c = ++a + a++ + --b + b-- + a + b++ + b;
+// console.log(c);
+
+
+// CONCATENATION
+// let name1= "Zainab Lakho";
+// let age = 21;
+// //__________first way to print____________
+// // console.log('My name is '+name1+' and my age is ' +age);
+// //__________second way to print___________
+// console.log(`My name is ${name1} and my age is ${age}`);
+
+
+
+
+
+
+
+
+
