b4b5f8d70d065bb211a646caa47891aa1e30d2cd
Zainab Lakho <130156475+ZLakho@users.noreply.github.com>
2024-06-22 01:10:40 +0500

    Add files via upload

---
+++ fifth_class.ts
@@ -0,0 +1,59 @@
+// ternary operator
+// let isHungry = true
+// let condition = isHungry ? "apple" : "banana"
+// console.log(`The result is ${condition}`)
+
+// let age1 = 19;
+// let condition = age1 >= 18 ? "allowed" : "not allowed"
+// console.log(condition)
+
+ // swich statement
+// let dayoff = "sunday";
+
+// functions
+// console.log a function used for printing results or queries
+// function greet(){
+//     return "Hi Zainab! kesi ho ?"
+// }
+// let mymessage = greet()
+// console.log(mymessage)
+
+// function with 1 argument
+// function sum  ( num : number ){
+//     return num + 10;
+// }
+// let summing = sum(45)
+// console.log(summing)
+
+// // multiplication
+// function multiplicaions  ( num : number , num1 : number){
+//     return num + num1;
+// }
+// let multiplicaion = multiplicaions(45 , 78)
+// console.log("Multiplication of two numbers is : " + multiplicaion)
+// /// division
+// function divisions  ( num : number , num1 : number){
+//     return num / num1;
+// }
+// let division = divisions(45 , 78)
+// console.log("Division of two numbers is : " +division)
+// // subtraction
+// function subtraction  ( num : number , num1 : number){
+//     return num - num1;
+// }
+// let sub = subtraction(45 , 78)
+// console.log("Subtraction of two numbers is : " +sub)
+// //addition
+// function addition  ( num : number , num1 : number){
+//     return num + num1;
+// }
+// let add = addition(45 , 78)
+// console.log("Addition of two numbers is : " +add)
+// // default
+// function Default  ( num : number , num1 : number){
+//     num1 = 33;
+//     return num + num1;
+// }
+// let def = Default(45 , 78)
+// console.log("Default values are of two numbers is : " +def)
+
